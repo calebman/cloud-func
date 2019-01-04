b5a586e192fc4ed42764bad016daa0a8b0213d28
chenjianhui <chenjianhui0428@gmail.com>
2019-01-04 17:12:14 +0800

    update bash

--- bin/stop.sh
+++ bin/stop.sh
@@ -4,6 +4,7 @@ pid=`ps -ef | grep cloud-func.jar | grep -v grep | awk '{print $2}'`
 if [ -n "$pid" ]
 then
    kill -9 $pid
+   echo application stop success
 else
    echo application already stop
 fi
