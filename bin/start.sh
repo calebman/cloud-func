69f509e7462623b9ccc6595fb6f3ac6ad02f5de2
chenjianhui <chenjianhui0428@gmail.com>
2019-01-04 15:30:04 +0800

    update bin

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-nohup java -jar /home/app/application/cloud-func.jar > /home/app/application/server.log 2>1&
-tail -f /home/app/application/server.log
+APP_NAME=cloud-func.jar
+export BIN_PATH=$(cd `dirname $0`;pwd)
+echo BIN_PATH:[$BIN_PATH]
+cd $BIN_PATH
+nohup java -jar $APP_NAME > server.log 2>1&
+tail -f server.log
--- bin/stop.sh
+++ bin/stop.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-echo "Stopping cloud-func Application"
+echo stopping application cloud-func
 pid=`ps -ef | grep cloud-func.jar | grep -v grep | awk '{print $2}'`
 if [ -n "$pid" ]
 then
    kill -9 $pid
+else
+   echo application already stop
 fi
