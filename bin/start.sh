bdf751c07293d3839a4da2ccc56e2d972381ecd0
chenjianhui <chenjianhui0428@gmail.com>
2019-01-04 17:10:46 +0800

    update bash

--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sh ./stop.sh
 APP_NAME=cloud-func.jar
 export BIN_PATH=$(cd `dirname $0`;pwd)
+sh $BIN_PATH/stop.sh
 echo BIN_PATH:[$BIN_PATH]
 cd $BIN_PATH
 nohup java -jar $APP_NAME > server.log 2>1&
